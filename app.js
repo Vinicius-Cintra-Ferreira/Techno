e433f6a01991516b35efbc9cd7a9851b773c2b07
Vinicius Cintra Ferreira <vinicintra3101@outlook.com>
2024-04-04 20:24:23 -0300

    Filter e Pré Modal

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ const vm = new Vue({
     carrinho: [],
     carrinhoAtivo: false,
   },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+    }
+  },
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json")
         .then(r => r.json())
         .then(data => {this.produtos = data});
     },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(data => {this.produto = data});
+    },
+    closeModal() {
+      this.produto = false;
+    }
   },
   created() {
     this.fetchProdutos();
